932ef75e7767d2bc4f4fefb1aaaeeb809d056718
andres peterson <andres.von.peterson@gmail.com>
2020-11-29 13:19:26 +0200

    webpacker ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,20 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import './add_question'
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("jquery")
 require("channels")
+
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+// require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
