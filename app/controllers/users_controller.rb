3ef8c9bc846c6e720738262517253575d3da2162
andres peterson <andres.von.peterson@gmail.com>
2020-12-03 20:33:53 +0200

    sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
 
   before_action :load_user, except: [:index, :create, :new]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
 
   def index
     @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new
   end
 
   def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new(user_params)
     if @user.save
       redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
@@ -33,9 +36,14 @@ class UsersController < ApplicationController
   def show
     @questions = @user.questions.order(created_at: :desc)
     @new_question = @user.questions.build
+    # @word_forms = %w[вопрос вопроса вопросов]
   end
 
   private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
 
   def load_user
     @user ||= User.find params[:id]
