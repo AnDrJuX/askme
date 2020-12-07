1f3e5f804b667840e15285917e7267fb69469652
andres peterson <andres.von.peterson@gmail.com>
2020-12-07 18:43:29 +0200

    removed comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,6 @@ module ApplicationHelper
   end
 
   def inclination(number, one, few, many)
-    # return "" if number.nil? || !number.is_a?(Numeric)
     balance = number % 10
     balance_100 = number % 100
 
@@ -25,10 +24,3 @@ module ApplicationHelper
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
 end
-
-
-# Добавьте на страницу пользователя блок с количеством вопросов.
-# Слово вопросов должно быть в правильной форме (как мы делали в уроке про «склонятор»).
-# В качестве ответа приложите ссылку на репозиторий с приложением askme.
-# Добавьте в ваше приложение метод склонятора в качестве хелпера.
-# Чтобы проверить, как он работает, поменяйте количество вопросов в болванке @questions.
